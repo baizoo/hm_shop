c9cbc188f790121d0624d286d0bbceea4cf5ebcb
baizoo <49893379+baizoo@users.noreply.github.com>
2026-01-21 15:14:23 +0800

    实现轮播图

--- lib/components/Home/HmSlider.dart
+++ lib/components/Home/HmSlider.dart
@@ -1,20 +1,36 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:hm_shop/viewmodels/Home.dart';
 
 class Hmslider extends StatefulWidget {
-  Hmslider({Key? key}) : super(key: key);
+  final List<BannerItem> banners;
+
+  Hmslider({Key? key,required this.banners}) : super(key: key);
 
   @override
   _HmsliderState createState() => _HmsliderState();
 }
 
 class _HmsliderState extends State<Hmslider> {
+  Widget _getSlider(){
+    //flutter中获取屏幕宽度的方法
+    final double screenWidth = MediaQuery.of(context).size.width; // 屏幕宽度
+
+    return CarouselSlider(items: List.generate(widget.banners.length, (int index){
+      return Image.network(widget.banners[index].imgUrl!,fit: BoxFit.cover,width: screenWidth,);
+    }), options: CarouselOptions(viewportFraction: 1,autoPlay: true,height: screenWidth*0.5));
+  }
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
-      height: 300,
-      alignment: Alignment.center,
-       child: Text("轮播图",style: TextStyle(fontSize: 20,color: Colors.white),),
+    //stack -> 轮播图 搜索框 指示灯导航
+    return Stack(
+      children: [_getSlider()],
     );
+    // return Container(
+    //   color: Colors.blue,
+    //   height: 300,
+    //   alignment: Alignment.center,
+    //    child: Text("轮播图",style: TextStyle(fontSize: 20,color: Colors.white),),
+    // );
   }
 }
--- lib/pages/Home/index.dart
+++ lib/pages/Home/index.dart
@@ -4,6 +4,7 @@ import 'package:hm_shop/components/Home/HmHot.dart';
 import 'package:hm_shop/components/Home/HmMoreList.dart';
 import 'package:hm_shop/components/Home/HmSlider.dart';
 import 'package:hm_shop/components/Home/HmSuggestion.dart';
+import 'package:hm_shop/viewmodels/Home.dart';
 
 class HomeView extends StatefulWidget {
   HomeView({Key? key}) : super(key: key);
@@ -13,9 +14,15 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  final List<BannerItem> _banners = [
+    BannerItem(id: "1", imgUrl: "https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/meituan/1.jpg"),
+    BannerItem(id: "2", imgUrl: "https://risun-tec.cn/ueditor/net/upload/image/20181117/6367806312985937509418879.jpg"),
+    BannerItem(id: "3", imgUrl: "https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/meituan/3.jpg"),
+  ];
+
   List<Widget> _getScrollChildren(){
     return[
-      SliverToBoxAdapter(child: Padding(padding: EdgeInsets.symmetric(horizontal: 10,),child: Hmslider())),//轮播图组件
+      SliverToBoxAdapter(child: Padding(padding: EdgeInsets.symmetric(horizontal: 10,),child: Hmslider(banners: _banners,))),//轮播图组件
       SliverToBoxAdapter(child: SizedBox(height: 10,),),//间距组件
       SliverToBoxAdapter(child: Hmcategory()),//更多列表组件
       SliverToBoxAdapter(child: SizedBox(height: 10,),),//间距组件
